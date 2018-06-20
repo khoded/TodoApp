8f858dfa36c03eb11b23be5c5c25acdbeabe0211
khoded <khodedng@yahoo.com>
2018-06-20 19:51:32 +0000

    passed data via ejs

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -1,7 +1,9 @@
+var data = [{item: 'get milk'},{item: 'walk dog'}, {item: 'write some code'}];
+
 module.exports = function (app) {
 
 app.get('/todo', function (req, res) {
-res.render('todo');
+res.render('todo', {todos: data});
 })
 
 app.post('/todo', function (req, res) {
