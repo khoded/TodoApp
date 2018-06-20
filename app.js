b80ed250785c0021c0db7c1af56011253b3b4c3c
khoded <khodedng@yahoo.com>
2018-06-20 21:05:08 +0000

    added data via ajax and delete data functionality

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var todoController=require('./controllers/todocontroller.js');
 var app = express();
-var bodyParser = require("body-parser"); 
+ 
 //set up template engine
 app.set('view engine', 'ejs');
 //static files
 app.use( express.static(__dirname + '/public'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+
 //fire controllers
 todoController(app);
 
