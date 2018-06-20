f6852a952d823ed8963d4f3cf4e38a0280870bf9
khoded <khodedng@yahoo.com>
2018-06-20 09:44:05 +0000

    added tools and contrllers

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var todoController=require('./controllers/todocontroller.js');
+var app = express();
+//set up template engine
+app.set('view engine', 'ejs');
+//static files
+app.use( express.static(__dirname + '/public'));
+
+//fire controllers
+todoController(app);
+
+//listen to port
+app.listen(3000);
+console.log(' Server listening on 3000');
---
+++ controllers/todocontroller.js
@@ -0,0 +1,16 @@
+module.exports = function (app) {
+
+app.get('/todo', function (req, res) {
+
+})
+
+app.post('/todo', function (req, res) {
+
+})
+
+app.delete('/todo', function (req, res) {
+ 
+})
+
+
+};
