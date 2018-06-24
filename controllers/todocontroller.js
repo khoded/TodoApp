edfd2b6a8d2b3969ff8e0e574b5f53b410c873ce
khoded <khodedng@yahoo.com>
2018-06-24 19:00:50 +0000

    made deletions

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -16,12 +16,14 @@ app.post('/todo', urlencodedParser, function (req, res) {
   res.render('todo', {todos: data});
 });
 
-app.delete('/todo:item', function (req, res) {
+app.delete('/todo/:item', function (req, res) {
   data = data.filter(function(todo) {
-        return todo.item.replace(/ /g, '-') !==  req.params.item
-        res.render('todo', {todos: data});
+        return todo.item.replace(/ /g, '-') !==  req.params.item;
+
     });
-  })
+    res.render('todo', {todos: data});
+  // res.json(data)
+  });
 
 
 };
